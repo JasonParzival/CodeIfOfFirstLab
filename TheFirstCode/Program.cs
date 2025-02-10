8608153ca0366a6562698664ffe575b7e3518cbf
JasonParzival <ruslanmuhamed123@gmail.com>
2025-02-10 22:08:22 +0800

    Добавьте файлы проекта.

---
+++ TheFirstCode/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace TheFirstCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var smth = 523341;
+            string str = smth.ToString();
+            var first_eq = (int)Char.GetNumericValue(str[0]) + (int)Char.GetNumericValue(str[1]) + (int)Char.GetNumericValue(str[2]);
+            var second_eq = (int)Char.GetNumericValue(str[3]) + (int)Char.GetNumericValue(str[4]) + (int)Char.GetNumericValue(str[5]);
+            if (first_eq == second_eq)
+            {
+                Console.WriteLine("Заданное число: " + str + " является счастливым!  Первая сумма: " + first_eq + " равна второй сумме: " + second_eq);
+            }
+            else if (first_eq > second_eq)
+            {
+                Console.WriteLine("Заданное число: " + str + " не является счастливым!  Первая сумма: " + first_eq + " больше второй суммы: " + second_eq);
+            }
+            else if (first_eq < second_eq)
+            {
+                Console.WriteLine("Заданное число: " + str + " не является счастливым!  Первая сумма: " + first_eq + " меньше второй суммы: " + second_eq);
+            }
+        }
+    }
+}

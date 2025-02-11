82f33facfef6d4a50088463689d775b08d259bd3
JasonParzival <ruslanmuhamed123@gmail.com>
2025-02-11 22:53:03 +0800

    Провел декомпозицию

--- TheFirstCode/Program.cs
+++ TheFirstCode/Program.cs
@@ -1,26 +1,41 @@
 ﻿namespace TheFirstCode
 {
+    public class Logic
+    {
+        public static int Get_sum(int smth, int choice) 
+        {
+            string str = smth.ToString();
+            var eq = (int)Char.GetNumericValue(str[choice]) + (int)Char.GetNumericValue(str[choice + 1]) + (int)Char.GetNumericValue(str[choice + 2]);
+            return eq;
+        }
+
+        public static string Message(int smth, int first, int second)
+        {
+            string str = smth.ToString();
+            string message = "";
+            if (first == second)
+            {
+                message = "Заданное число: " + str + " является счастливым!  Первая сумма: " + first + " равна второй сумме: " + second;
+            }
+            else if (first > second)
+            {
+                message = "Заданное число: " + str + " не является счастливым!  Первая сумма: " + first + " больше второй суммы: " + second;
+            }
+            else if (first < second)
+            {
+                message = "Заданное число: " + str + " не является счастливым!  Первая сумма: " + first + " меньше второй суммы: " + second;
+            }
+            return message;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
             var smth = 523341;
-            string str = smth.ToString();
-            var first_eq = (int)Char.GetNumericValue(str[0]) + (int)Char.GetNumericValue(str[1]) + (int)Char.GetNumericValue(str[2]);
-            var second_eq = (int)Char.GetNumericValue(str[3]) + (int)Char.GetNumericValue(str[4]) + (int)Char.GetNumericValue(str[5]);
-            string message = "";
-            if (first_eq == second_eq)
-            {
-                message = "Заданное число: " + str + " является счастливым!  Первая сумма: " + first_eq + " равна второй сумме: " + second_eq;
-            }
-            else if (first_eq > second_eq)
-            {
-                message = "Заданное число: " + str + " не является счастливым!  Первая сумма: " + first_eq + " больше второй суммы: " + second_eq;
-            }
-            else if (first_eq < second_eq)
-            {
-                message = "Заданное число: " + str + " не является счастливым!  Первая сумма: " + first_eq + " меньше второй суммы: " + second_eq;
-            }
+            var first_eq = Logic.Get_sum(smth, 0);
+            var second_eq = Logic.Get_sum(smth, 3);
+            string message = Logic.Message(smth, first_eq, second_eq);       
 
             Console.WriteLine(message);
         }
